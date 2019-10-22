6255db04f945631c746db2a59a3571783ddb75e1
bulat <damask161092@gmail.com>
2019-10-22 16:39:17 +0500

    almost ready

--- Untitled-1.js
+++ Untitled-1.js
@@ -13,62 +13,82 @@ var sek         = 0;
 var prevArrow   = divs.length -1;
 var divStyle    = 'char';
 
-divs[s].style.right = '0';
+ 
 dot[s].style.transform = 'scale(1.2)';
-dot[s].style.background = 'rgba(0, 0, 0, 0.6)';
+dot[s].style.background = 'rgba(0, 0, 0, 1)';
+slider.style.transition = 'ease-in-out 1s';
 
         
 
         
-        next.onclick = function(){
-            slider.style.right = '100%';
-            dot[s].style.transform = 'scale(1)';
-            dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
-            s++;
-            if(s == divs.length ){
-                s--;
+next.onclick = function(){
+            
+    dot[s].style.transform = 'scale(1)';
+    dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
+    s++;
+        if(s == divs.length ){
+            s--;
                 for(s; s > 0; s-- ){
-                    divs[s].style.right = '100%'; 
+                    
                     dot[s].style.transform = 'scale(1)';
                     dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
                 }
+        }
+    slider.style.left = `-${100*s}%`;
+    
+    nextArrow++;
+            if(nextArrow > divs.length-1 ){
+                nextArrow = 0;     
             }
-            slider.style.left = '-100*s}%';
-            
+            divStyle = window.getComputedStyle(divs[nextArrow]);
+            next.style.backgroundImage = divStyle.getPropertyValue('background-image');  
+
+
             prevArrow++;
-            if(prevArrow > divs.length ){
+            if(prevArrow > divs.length-1 ){
                 prevArrow = 0;     
             }
-            divStyle = window.getComputedStyle(divs[prevArrow])
-            next.style.backgroundImage = divStyle.getPropertyValue('background-image');  
-            divs[s].style.right = '0';
+            divStyle = window.getComputedStyle(divs[prevArrow]);
+            prev.style.backgroundImage = divStyle.getPropertyValue('background-image');  
+
+
+
+            
             dot[s].style.transform = 'scale(1.2)';
-            dot[s].style.background = 'rgba(0, 0, 0, 0.6)';
+            dot[s].style.background = 'rgba(0, 0, 0, 1)';
         }
         prev.onclick = function(){
-            divs[s].style.right = '100%';
+            
             dot[s].style.transform = 'scale(1)';
             dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
             s--;
             if(s < 0 ){
                 s++;
                 for(s; s < divs.length-1; s++ ){
-                    divs[s].style.right = '-100%';
+                     
                     dot[s].style.transform = 'scale(1)';
-                     dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
+                    dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
                 }
             }
-            
+            slider.style.left = `-${100*s}%`;
+
             prevArrow--;
             if(prevArrow < 0 ){
                 prevArrow = divs.length - 1;     
             }
-            divStyle = window.getComputedStyle(divs[prevArrow])
-            prev.style.backgroundImage = divStyle.getPropertyValue('background-image');  
-            divs[s].style.right = '0';
+            divStyle = window.getComputedStyle(divs[prevArrow]);
+            prev.style.backgroundImage = divStyle.getPropertyValue('background-image'); 
+            
+            
+            nextArrow--;
+            if(nextArrow < 0 ){
+                nextArrow = divs.length - 1;     
+            }
+            divStyle = window.getComputedStyle(divs[nextArrow]);
+            next.style.backgroundImage = divStyle.getPropertyValue('background-image');     
+
+ 
             dot[s].style.transform = 'scale(1.2)';
-            dot[s].style.background = 'rgba(0, 0, 0, 0.6)';
-        }
-
-//setInterval(next.onclick, 10000);
-
+            dot[s].style.background = 'rgba(0, 0, 0, 1)';
+}
+ 
