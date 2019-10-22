a9132523cff33e8496da104cf26d9ede1fdfeaa8
bulat <damask161092@gmail.com>
2019-10-22 11:03:27 +0500

    second commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,23 +1,27 @@
     "use strict";
 
-        var next = document.getElementById('next');
-        var prev = document.getElementById('prev');
-        var divs = document.querySelectorAll('.slid');
-        var dot = document.querySelectorAll('.dot');
-        
-        var s = 0;
+var next    = document.getElementById('next');
+var prev    = document.getElementById('prev');
+var divs    = document.querySelectorAll('.slid');
+var dot     = document.querySelectorAll('.dot');
+var slider  = document.getElementById('slider');
 
-        var sek = 0;
 
-        divs[s].style.right = '0';
-        dot[s].style.transform = 'scale(1.2)';
-        dot[s].style.background = 'rgba(0, 0, 0, 0.6)';
+var s           = 0;
+var nextArrow   = 1;
+var sek         = 0;
+var prevArrow   = divs.length -1;
+var divStyle    = 'char';
+
+divs[s].style.right = '0';
+dot[s].style.transform = 'scale(1.2)';
+dot[s].style.background = 'rgba(0, 0, 0, 0.6)';
 
         
 
         
         next.onclick = function(){
-            divs[s].style.right = '-100%';
+            slider.style.right = '100%';
             dot[s].style.transform = 'scale(1)';
             dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
             s++;
@@ -29,11 +33,19 @@
                     dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
                 }
             }
+            slider.style.left = '-100*s}%';
+            
+            prevArrow++;
+            if(prevArrow > divs.length ){
+                prevArrow = 0;     
+            }
+            divStyle = window.getComputedStyle(divs[prevArrow])
+            next.style.backgroundImage = divStyle.getPropertyValue('background-image');  
             divs[s].style.right = '0';
             dot[s].style.transform = 'scale(1.2)';
             dot[s].style.background = 'rgba(0, 0, 0, 0.6)';
         }
-        prev.onclick = function(){Z
+        prev.onclick = function(){
             divs[s].style.right = '100%';
             dot[s].style.transform = 'scale(1)';
             dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
@@ -46,10 +58,17 @@
                      dot[s].style.background = 'rgba(0, 0, 0, 0.4)';       
                 }
             }
+            
+            prevArrow--;
+            if(prevArrow < 0 ){
+                prevArrow = divs.length - 1;     
+            }
+            divStyle = window.getComputedStyle(divs[prevArrow])
+            prev.style.backgroundImage = divStyle.getPropertyValue('background-image');  
             divs[s].style.right = '0';
             dot[s].style.transform = 'scale(1.2)';
             dot[s].style.background = 'rgba(0, 0, 0, 0.6)';
         }
 
-setInterval(next.onclick, 10000);
+//setInterval(next.onclick, 10000);
 
